dfe16aeab3e235f18d224727edc9535ac52a79e7
rezki <rezki.pratama@pactindo.com>
2023-09-11 11:42:23 +0700

    ubah port api

--- src/service/server.js
+++ src/service/server.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import FunctionService from '../tools/FunctionService';
 // import FunctionService from '../tools/FunctionService';
 
-const baseURL = 'http://156.67.219.199:80/api/v1';
+const baseURL = 'http://156.67.219.199:8087/api/v1';
 
 const AXIOS = axios.create({
   baseURL
